8aa0e02e5597a6d563bd32f7f4805af7f5f40011
boisset-c <boissetcosme@gmail.com>
2022-12-21 19:18:01 -0600

    Setup basic server functionalities.

---
+++ server/server.js
@@ -0,0 +1,28 @@
+import fastify from "fastify";
+import sensible from "@fastify/sensible";
+import dotenv from "dotenv";
+import { PrismaClient } from "@prisma/client";
+dotenv.config();
+
+const app = fastify();
+app.register(sensible);
+const prisma = new PrismaClient();
+
+app.get("/posts", async (req, res) => {
+  return await commentToDb(
+    prisma.post.findMany({
+      select: {
+        id: true,
+        title: true,
+      },
+    })
+  );
+});
+
+async function commentToDb(promise) {
+  const [error, data] = await app.to(promise);
+  if (error) return app.httpErrors.internalServerError(error.message);
+  return data;
+}
+
+app.listen({ port: process.env.PORT });
