4dbd03ce46cb85190fa74461a10c086ce595dcf4
boisset-c <boissetcosme@gmail.com>
2022-12-24 10:21:44 -0600

    Rendered Comments Components.

--- client/src/context/PostContext.jsx
+++ client/src/context/PostContext.jsx
@@ -1,17 +1,39 @@
-import React from "react";
+import React, { useContext, useMemo } from "react";
 import { useAsync } from "../hooks/useAsync";
 import { getPost } from "../services/post";
 import { useParams } from "react-router-dom";
 
 const Context = React.createContext();
 
+export function usePost() {
+  return useContext(Context);
+}
+
 export function PostProvider({ children }) {
   const { id } = useParams();
   const { loading, error, value: post } = useAsync(() => getPost(id), [id]);
-  console.log(post);
-  console.log(children);
+  const commentsByParentId = useMemo(() => {
+    if (post?.comments == null) return [];
+    const group = {};
+    post.comments.forEach((comment) => {
+      group[comment.parentId] ||= [];
+      group[comment.parentId].push(comment);
+    });
+    return group;
+  }, [post?.comments]);
+
+  function getReplies(parentId) {
+    return commentsByParentId[parentId];
+  }
+
   return (
-    <Context.Provider value={{ id, ...post }}>
+    <Context.Provider
+      value={{
+        post: { id, ...post },
+        rootComments: commentsByParentId[null],
+        getReplies,
+      }}
+    >
       {loading ? (
         <h1>Loading...</h1>
       ) : error ? (
