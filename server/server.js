42186eb704ba7082a1b83a8502a2ee7b59920a54
boisset-c <boissetcosme@gmail.com>
2022-12-25 14:10:53 -0600

    Phase one of project finished.

--- server/server.js
+++ server/server.js
@@ -53,21 +53,44 @@ app.get("/posts", async (req, res) => {
 
 app.get("/posts/:id", async (req, res) => {
   return await commitToDb(
-    prisma.post.findUnique({
-      where: {
-        id: req.params.id,
-      },
-      select: {
-        body: true,
-        title: true,
-        comments: {
-          orderBy: {
-            createdAt: "desc",
-          },
-          select: COMMENT_SELECT_FIELDS,
+    prisma.post
+      .findUnique({
+        where: {
+          id: req.params.id,
         },
-      },
-    })
+        select: {
+          body: true,
+          title: true,
+          comments: {
+            orderBy: {
+              createdAt: "desc",
+            },
+            select: {
+              ...COMMENT_SELECT_FIELDS,
+              _count: { select: { likes: true } },
+            },
+          },
+        },
+      })
+      .then(async (post) => {
+        const likes = await prisma.like.findMany({
+          where: {
+            userId: req.cookies.userId,
+            commentId: { in: post.comments.map((comment) => comment.id) },
+          },
+        });
+        return {
+          ...post,
+          comments: post.comments.map((comment) => {
+            const { _count, ...commentFields } = comment;
+            return {
+              ...commentFields,
+              likedByMe: likes.find((like) => like.commentId === comment.id),
+              likeCount: _count.likes,
+            };
+          }),
+        };
+      })
   );
 });
 
@@ -77,15 +100,23 @@ app.post("/posts/:id/comments", async (req, res) => {
   }
 
   return await commitToDb(
-    prisma.comment.create({
-      data: {
-        message: req.body.message,
-        userId: req.cookies.userId,
-        parentId: req.body.parentId,
-        postId: req.params.id,
-      },
-      select: COMMENT_SELECT_FIELDS,
-    })
+    prisma.comment
+      .create({
+        data: {
+          message: req.body.message,
+          userId: req.cookies.userId,
+          parentId: req.body.parentId,
+          postId: req.params.id,
+        },
+        select: COMMENT_SELECT_FIELDS,
+      })
+      .then((comment) => {
+        return {
+          ...comment,
+          likeCount: 0,
+          likedByMe: false,
+        };
+      })
   );
 });
 
@@ -136,6 +167,30 @@ app.delete("/posts/:postId/comments/:commentId", async (req, res) => {
       select: { id: true },
     })
   );
+});
+
+app.post("/posts/:postId/comments/:commentId/toggleLike", async (req, res) => {
+  const data = {
+    commentId: req.params.commentId,
+    userId: req.cookies.userId,
+  };
+
+  const like = await prisma.like.findUnique({
+    where: {
+      userId_commentId: data,
+    },
+  });
+  if (like == null) {
+    return await commitToDb(prisma.like.create({ data })).then(() => {
+      return { addLike: true };
+    });
+  } else {
+    return await commitToDb(
+      prisma.like.delete({ where: { userId_commentId: data } })
+    ).then(() => {
+      return { addLike: false };
+    });
+  }
 });
 
 async function commitToDb(promise) {
